a1ddf46c31066ee311b5d2f2f4c90cbcc3b74e60
Kola Prabhas <kolaprabhas2310@gmail.com>
2024-12-12 23:12:05 +0530

    Removed hardcoded url

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -70,7 +70,7 @@ export default function CheckoutForm({ orderId, totalAmount }) {
 			elements,
 			confirmParams: {
 				// Make sure to change this to your payment completion page
-				return_url: `http://localhost:3000/order-details/${orderId}`,
+				return_url: `/order-details/${orderId}`,
 			},
 		});
 
